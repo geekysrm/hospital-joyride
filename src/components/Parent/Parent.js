661bad433a90efa45dfe5a5374aba4f8e5de87a5
K Sai Kishan <sonusaikishan@gmail.com>
2019-09-05 00:22:24 +0530

    Lesson learned today : Never do api call without ACTION FIREgit add .!

--- src/components/Parent/Parent.js
+++ src/components/Parent/Parent.js
@@ -9,26 +9,82 @@ import {
 } from "react-redux-firebase";
 import { getFirestore } from "redux-firestore";
 
-import { Icon } from "semantic-ui-react";
+import { Icon, Placeholder, Segment, Button } from "semantic-ui-react";
 
 import "./Parent.css";
 
 class Parent extends Component {
+  renderPastTreatments = () => {
+    let past = [];
+    const firestore = getFirestore();
+    firestore
+      .collection("treatments")
+      .where("isCompleted", "==", false)
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(doc => {
+          past.push(doc.data());
+        });
+      });
+  };
+
   render() {
     return (
-      <div>
+      <div className="page-container">
         {this.props.parents ? (
-          <div className="header">
-            <div>
-              <Icon name="user" circular size="big" />
+          <>
+            <div className="header">
+              <div>
+                <Icon name="user" circular size="big" />
+              </div>
+              <div style={{ paddingLeft: "30px" }}>
+                <div className="header-name">{this.props.parents[0].name}</div>
+                <div className="header-email">
+                  {this.props.parents[0].email}
+                </div>
+              </div>
             </div>
-            <div style={{ paddingLeft: "30px" }}>
-              <div className="header-name">{this.props.parents[0].name}</div>
-              <div className="header-email">{this.props.parents[0].email}</div>
+
+            <div style={{ paddingTop: "15px", paddingBottom: "50px" }}>
+              <div className="heading">
+                Your Childrens Current Treatments -{" "}
+              </div>
+              <div style={{ paddingTop: "15px" }}>
+                <Segment>
+                  Your childrens don't have any current treatments
+                </Segment>
+              </div>
             </div>
-          </div>
+
+            <div style={{ paddingTop: "15px", paddingBottom: "50px" }}>
+              <div className="heading">Your Childrens Past Treatments - </div>
+              <div style={{ paddingTop: "15px" }}>
+                {this.renderPastTreatments()}
+              </div>
+            </div>
+            <div className="footer">
+              <Button fluid primary>
+                Get Help !!
+              </Button>
+            </div>
+          </>
         ) : (
-          <div>loading ...</div>
+          <div style={{ padding: "20px" }}>
+            <Placeholder>
+              <Placeholder.Paragraph>
+                <Placeholder.Line />
+                <Placeholder.Line />
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Paragraph>
+              <Placeholder.Paragraph>
+                <Placeholder.Line />
+                <Placeholder.Line />
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Paragraph>
+            </Placeholder>
+          </div>
         )}
       </div>
     );
