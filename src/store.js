2f60bafa630759a900ff6c4f5626b23f68c1adeb
amiya-1998 <amiya.tripathy1998@gmail.com>
2019-09-08 22:47:36 +0530

    removed devtools

--- src/store.js
+++ src/store.js
@@ -47,7 +47,7 @@ const store = createStoreWithFirebase(
   initialState,
   compose(
     reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
